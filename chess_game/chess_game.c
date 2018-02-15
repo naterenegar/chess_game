5aec1d9e3a999702fad0497e42d08cd4061c534f
naterenegar <naterenegar@gmail.com>
2018-02-15 18:16:00 -0500

    Centered player board and added pieces

--- chess_game/chess_game.c
+++ chess_game/chess_game.c
@@ -1,77 +1,63 @@
-#include <stdio.h>
 #include <ncurses.h>
 
-#define BOARD_WIDTH 8
-#define BOARD_LENGTH 8
+#define W_FIRST 1
+#define Y_FIRST 2
+
+char p1_r1[8] = {'r', 'h', 'b', 'k', 'q', 'b', 'h', 'r'};
+char p1_ps[8] = {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'};	 
+char p2_r1[8] = {'r', 'h', 'b', 'k', 'q', 'b', 'h', 'r'};
+char p2_ps[8] = {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'};	 
+
+int pos_p1[16];
+int pos_p2[16];
 
 
-void clearScreen(void) {
-	printf("\033[2J");
-}
-
-void setBoard(char board[][8]){
-	int i, j;
-	for(i = 0; i < BOARD_LENGTH; i++) {
-		for(j = 0; j < BOARD_WIDTH; j++) {
-			if(i == 0 || i == 7) {
-				switch(j) { 
-					case 0: board[i][j] = 'R';
-						break;
-					case 1: board[i][j] = 'k'; 
-						break;
-					case 2: board[i][j] = 'B';
-						break;
-					case 3: board[i][j] = 'K';
-						break;
-					case 4: board[i][j] = 'Q';
-						break;
-					case 5: board[i][j] = 'B';
-						break;
-					case 6: board[i][j] = 'k';
-						break;
-					case 7: board[i][j] = 'R';
-						break;
-				 }
-			} else if(i == 1 || i == 6) {
-				board[i][j] = 'P';
-			} else {
-				board[i][j] = '-';
-			}
-		}
+void changeState(int *s){
+	if(*s == W_FIRST) {
+		*s = Y_FIRST;	
+	} else {
+		*s = W_FIRST;
 	}
 }
 
-void printBoard(char board[][8]) {
-	int i, j;	
-	for(i = 0; i <= BOARD_LENGTH*2; i++) {
-		for(j = 0; j < BOARD_WIDTH; j++) {
-			if(i % 2 == 0 && j != 7) {
-				printw("-----");
-			} else if(i % 2 == 0) {
-				printw("--");
-			} else {
-				printw("| %c ", board[i/2][j]); 
-			if(j == 7) {
-					printw("| %c |", board[i/2][j]);
-				}
-			}
-		}
-		printw("\n");	
-	}	
-}
-
 int main(void) {
-	char board[BOARD_LENGTH][BOARD_WIDTH];
+	int i, j, state = Y_FIRST, *s = &state, row, col;
 	
-	initscr(); 
-	cbreak();
 
-	mvprintw(15,30,"I like dick");
+	initscr();
+	start_color();
 	
-	setBoard(board);	
-	printBoard(board);	
+	getmaxyx(stdscr, row, col);
 
-	getch();	
+	init_pair(1, COLOR_BLUE, COLOR_WHITE);
+	init_pair(2, COLOR_BLUE, COLOR_YELLOW);
+	for(i = 0; i < 8; i++) { 	
+		for(j = 0; j < 8; j++) {
+			attron(COLOR_PAIR(state));
+			changeState(s);
+			move((i*3) + (row/2 -12), j*6 + (col/2 - 24));
+			printw("------");
+			move(((i*3)+1) + (row/2 -12), j*6 + (col/2 - 24));
+			if(i == 0) {
+				printw("--%c%d--", p1_r1[j], 1);
+			} else if(i == 1) {
+				printw("--%c%d--", p1_ps[j], 1);
+			} else if(i == 6) {
+				printw("--%c%d--", p2_ps[j], 2);
+			} else if(i == 7) {
+				printw("--%c%d--", p2_r1[j], 2);
+			} else {
+				printw("------");
+			}
+			move((i*3)+2 + (row/2 -12), j*6 + (col/2 - 24));
+			printw("------");
+		}
+		changeState(s);
+	}
+	
+	getch();
+
 	endwin();
+	
 	return 0;
 }
